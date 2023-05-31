fea0297204b21219ebcaef51e7b8296d1557ff98
AxelSantamariaR <axel.santamariar@ug.edu.ec>
2023-05-31 16:04:18 -0500

    update-directorio-component_actividades

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
+//Modules
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InicioComponentComponent} from './componentes/inicio-component/inicio-component.component';
 
-const routes: Routes = [];
+//Components
+import { InicioComponentComponent} from './components/inicio-component/inicio-component.component';
+import { ActividadesPageComponent } from './components/actividades-page/actividades-page.component';
+
+const routes: Routes = [
+  { path: '',                 redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home',             component: InicioComponentComponent },
+  { path: 'actividades',      component: ActividadesPageComponent },
+  { path: '**',               redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports:[
-    RouterModule.forRoot([
-      {path: '', component:InicioComponentComponent}
-    ])
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
+//Modules
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MaterialModule } from './shared/material.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponentComponent } from './componentes/nav-component/nav-component.component';
-import { MaterialModule } from './material/material.module';
-import { FooterComponent } from './componentes/footer/footer.component';
-import { InicioComponentComponent } from './componentes/inicio-component/inicio-component.component';
+
+//Components
+import { AppComponent } from './app.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NavComponentComponent } from './layouts/nav-component/nav-component.component';
+import { InicioComponentComponent } from './components/inicio-component/inicio-component.component';
+import { ActividadesPageComponent } from './components/actividades-page/actividades-page.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavComponentComponent,
     FooterComponent,
-    InicioComponentComponent
+    InicioComponentComponent,
+    ActividadesPageComponent
   ],
   imports: [
     BrowserModule,
