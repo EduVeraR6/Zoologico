298a8cdaeb3cc94588c8e9df8ab41c1eceea434e
Villamar Merchan <angelo.villamarm@ug.edu.ec>
2023-06-01 21:14:03 -0500

    nuevo

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD:src/app/material/material.module.ts
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input'
-=======
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
->>>>>>> 3f13587ae01a56e93608362e3d249a606fb0745f:src/app/shared/material.module.ts
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input'
 
 @NgModule({
   declarations: [],
@@ -19,18 +14,12 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   exports:[
     MatToolbarModule,
-<<<<<<< HEAD:src/app/material/material.module.ts
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
     MatIconModule,
-    MatInputModule
-  ],
-  imports: [
-    CommonModule
-=======
+    MatInputModule,
     MatButtonModule
->>>>>>> 3f13587ae01a56e93608362e3d249a606fb0745f:src/app/shared/material.module.ts
   ]
 })
 export class MaterialModule { }
